851a4fc417c4e7296f4b4e823169428ee1a47330
Felipe Lagos <felagossanchez@gmail.com>
2023-02-06 22:24:15 -0300

    stack navigator

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-import { NavigationContainer } from "@react-navigation/native";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
@@ -11,9 +10,7 @@ export default function App() {
     <QueryClientProvider client={queryClient}>
       <StatusBar style="auto" />
       <SafeAreaProvider>
-        <NavigationContainer>
-          <AppNavigator />
-        </NavigationContainer>
+        <AppNavigator />
       </SafeAreaProvider>
     </QueryClientProvider>
   );
--- src/navigator/AppNavigator.tsx
+++ src/navigator/AppNavigator.tsx
@@ -1,10 +1,13 @@
+import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { CategoriesScreen } from "../screens";
 
 const Stack = createNativeStackNavigator();
 
 export const AppNavigator = () => (
-  <Stack.Navigator>
-    <Stack.Screen name="Home" component={CategoriesScreen} />
-  </Stack.Navigator>
+  <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={CategoriesScreen} options={{ headerShown: false }} />
+    </Stack.Navigator>
+  </NavigationContainer>
 )
